4915a7bf19d0e1465e0638801e7e1da42a7ac1c7
sgutierrezos04 <sgutierrezos@unal.edu.co>
2023-03-05 15:50:55 -0500

    2ndo envio

--- src/main/java/classroom/Tipo.java
+++ src/main/java/classroom/Tipo.java
@@ -4,9 +4,7 @@ public enum Tipo {
 	DISCIPLINAR(10),FUNDAMENTACION(20),ELECTIVA(30);
 	
 	private int codigo;
-	private String nombre;
-	private Tipo(int codigo,String nombre) {
+	private Tipo(int codigo) {
 		this.codigo = codigo;
-		this.nombre = nombre;
 	}
 }
